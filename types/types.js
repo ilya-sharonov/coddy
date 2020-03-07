a6216a30cd8362340078033164cd742cda641958
Ilya Sharonov <sharonov@a-3.ru>
2020-03-07 13:47:17 +0300

    Enhanced HTML insertion function

--- types/types.js
+++ types/types.js
@@ -10,8 +10,8 @@ function createBlock(name, className) {
 
 (function() {
     const parent = document.querySelector('.parent');
-    parent.insertAdjacentHTML('beforeend', createBlock('object', 'object'));
-    parent.insertAdjacentHTML('beforeend', createBlock('boolean', 'boolean'));
-    parent.insertAdjacentHTML('beforeend', createBlock('string', 'string'));
-    parent.insertAdjacentHTML('beforeend', createBlock('number', 'number'));
+    const types = ['boolean', 'number', 'string', 'undefined', 'null', 'object'];
+    types.forEach(item => {
+        parent.insertAdjacentHTML('beforeend', createBlock(item, item));
+    });
 })();
